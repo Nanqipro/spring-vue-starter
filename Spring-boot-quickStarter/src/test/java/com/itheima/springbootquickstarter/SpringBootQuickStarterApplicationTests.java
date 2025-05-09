d992078c7763da5aef5fb1daa744edbbd0e262b8
nanqipro <2280285631@qq.com>
2025-05-09 15:19:00 +0800

    20250509

---
+++ Spring-boot-quickStarter/src/test/java/com/itheima/springbootquickstarter/SpringBootQuickStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootquickstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuickStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
