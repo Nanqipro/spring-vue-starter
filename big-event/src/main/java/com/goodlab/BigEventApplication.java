0e3876be0b0b231c2dd0b2620c16405f86c4add4
nanqipro <2280285631@qq.com>
2025-07-31 20:15:01 +0800

    1

---
+++ big-event/src/main/java/com/goodlab/BigEventApplication.java
@@ -0,0 +1,19 @@
+package com.goodlab;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class BigEventApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BigEventApplication.class, args);
+
+    }
+}
---
+++ big-event/src/main/java/com/goodlab/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.goodlab.service.impl;
+
+import com.goodlab.mapper.UserMapper;
+import com.goodlab.pojo.User;
+import com.goodlab.service.UserService;
+import com.goodlab.utils.Md5Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User findByUserName(String username) {
+        User u = userMapper.findByUserName(username);
+        return u;
+    }
+
+    @Override
+    public void register(String username, String password) {
+        // 加密
+        String md5String = Md5Util.getMD5String(password);
+
+        // 添加
+        userMapper.add(username, md5String);
+    }
+}
