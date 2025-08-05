00ab2c71f0d108e42deb8d102e39387f43111508
nanqipro <2280285631@qq.com>
2025-08-05 15:49:48 +0800

    1

--- big-event/src/main/java/com/goodlab/controller/FileUploadController.java
+++ big-event/src/main/java/com/goodlab/controller/FileUploadController.java
@@ -2,6 +2,7 @@ package com.goodlab.controller;
 
 
 import com.goodlab.pojo.Result;
+import com.goodlab.utils.AliOssUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,14 +15,16 @@ import java.util.UUID;
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public Result<String> upload(MultipartFile file) throws IOException {
+    public Result<String> upload(MultipartFile file) throws Exception {
 
         // 把文件的内容存储到本地磁盘上
         String originalFilename = file.getOriginalFilename();
         // 保证文件的名字唯一 防止文件覆盖
         String newFileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
-        file.transferTo(new File("C:/Users/nanqipro/Desktop/"+newFileName));
+        // file.transferTo(new File("C:/Users/nanqipro/Desktop/"+newFileName));
 
-        return Result.success("url访问地址：");
+        // 调用aliOssUtil上传文件
+        String url = AliOssUtil.uploadFile(newFileName, file.getInputStream());
+        return Result.success(url);
     }
 }
